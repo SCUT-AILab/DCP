8335e405e214af2280fa31b451daefd54e8074b4
JingLiu95 <liujing_95@outlook.com>
2019-05-17 17:31:39 +0800

    Add pruned vgg model.

--- dcp/models/__init__.py
+++ dcp/models/__init__.py
@@ -2,3 +2,4 @@ from .preresnet import *
 from .pruned_preresnet import *
 from .resnet import *
 from .pruned_preresnet import *
+from .pruned_vgg import *
---
+++ dcp/models/pruned_vgg.py
@@ -0,0 +1,60 @@
+import math
+
+import torch.nn as nn
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'A'],
+}
+
+__all__ = ["PrunedVGGCIFAR"]
+
+
+class PrunedVGGCIFAR(nn.Module):
+
+    def __init__(self, pruning_rate, depth=19, num_classes=10):
+        super(PrunedVGGCIFAR, self).__init__()
+        self.cfg = cfg['VGG' + str(depth)]
+        self.features = self.make_layers(self.cfg, pruning_rate)
+        self.classifier = nn.Linear(512, num_classes)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+    def make_layers(self, cfg, pruning_rate):
+        layers = []
+        in_channels = 3
+        for i, v in enumerate(cfg):
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            elif v == 'A':
+                layers += [nn.AvgPool2d(kernel_size=2, stride=2)]
+            else:
+                if i != len(cfg) - 2:
+                    v = int(v - math.floor(v * pruning_rate))
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                in_channels = v
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
